a3b3f46188ed7e3892aa896a2a00eb3cad6a6698
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2015-08-11 16:38:31 -0400

    Add code to call real external processes

--- scripts/pancancer.py
+++ scripts/pancancer.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import sys
+import subprocess
 import logging
 from cliff.app import App
 from cliff.commandmanager import CommandManager
@@ -11,20 +12,20 @@ class WorkflowLister:
     # Eventually, this data structure should come from some sort of online registry. For now, just define it in code.
     _workflows= {   'HelloWorld':
                     {
-                        'full_name':' 	Workflow_Bundle_HelloWorld_1.0-SNAPSHOT_SeqWare_1.1.1',
+                        'full_name':'Workflow_Bundle_HelloWorld_1.0-SNAPSHOT_SeqWare_1.1.1',
                         'http_workflow':
                         {
                             'url':'https://s3.amazonaws.com/oicr.workflow.bundles/released-bundles/Workflow_Bundle_HelloWorld_1.0-SNAPSHOT_SeqWare_1.1.1.zip',
                             'version':'1.0-SNAPSHOT'
                         },
-                        "containers":
+                        'containers':
                         {
-                            "seqware_whitestar_pancancer": {
-                                "name":"seqware_whitestar_pancancer",
-                                "image_name": "pancancer/seqware_whitestar_pancancer:1.1.1"
+                            'seqware_whitestar_pancancer': {
+                                'name':'seqware_whitestar_pancancer',
+                                'image_name': 'pancancer/seqware_whitestar_pancancer:1.1.1'
                             }
                         },
-                        "ami_id":"ami-12345"
+                        'ami_id':'ami-12345'
                     },
                     'Sanger':
                     {
@@ -34,14 +35,14 @@ class WorkflowLister:
                             'url':'https://s3.amazonaws.com/oicr.workflow.bundles/released-bundles/Workflow_Bundle_SangerPancancerCgpCnIndelSnvStr_1.0.8_SeqWare_1.1.0.zip',
                             'version':'1.0.8'
                         },
-                        "containers":
+                        'containers':
                         {
-                            "seqware_whitestar_pancancer": {
-                                "name":"seqware_whitestar_pancancer",
-                                "image_name": "pancancer/seqware_whitestar_pancancer:1.1.1"
+                            'seqware_whitestar_pancancer': {
+                                'name':'seqware_whitestar_pancancer',
+                                'image_name': 'pancancer/seqware_whitestar_pancancer:1.1.1'
                             }
                         },
-                        "ami_id":"ami-12345"
+                        'ami_id':'ami-12345'
                     },
                     'BWA':
                     {
@@ -51,14 +52,14 @@ class WorkflowLister:
                             'url':'https://s3.amazonaws.com/oicr.workflow.bundles/released-bundles/Workflow_Bundle_BWA_2.6.5_SeqWare_1.1.1.zip',
                             'version':'2.6.5'
                         },
-                        "containers":
+                        'containers':
                         {
-                            "seqware_whitestar_pancancer": {
-                                "name":"seqware_whitestar_pancancer",
-                                "image_name": "pancancer/seqware_whitestar_pancancer:1.1.1"
+                            'seqware_whitestar_pancancer': {
+                                'name':'seqware_whitestar_pancancer',
+                                'image_name': 'pancancer/seqware_whitestar_pancancer:1.1.1'
                             }
                         },
-                        "ami_id":"ami-12345"
+                        'ami_id':'ami-12345'
                     },
                     'DKFZ/EMBL':
                     {
@@ -69,26 +70,26 @@ class WorkflowLister:
 
                             'version':'1.0.5'
                         },
-                        "containers":
+                        'containers':
                         {
-                            "pcawg-delly-workflow": {
-                                "name":"pcawg-delly-workflow",
-                                "image_name": "pancancer/pcawg-delly-workflow:1.0"
+                            'pcawg-delly-workflow': {
+                                'name':'pcawg-delly-workflow',
+                                'image_name': 'pancancer/pcawg-delly-workflow:1.0'
                             },
-                            "seqware_whitestar_pancancer": {
-                                "name":"seqware_whitestar_pancancer",
-                                "image_name": "pancancer/seqware_whitestar_pancancer:1.1.1"
+                            'seqware_whitestar_pancancer': {
+                                'name':'seqware_whitestar_pancancer',
+                                'image_name': 'pancancer/seqware_whitestar_pancancer:1.1.1'
                             }
                         },
-                        "s3_containers":
+                        's3_containers':
                         {
-                            "dkfz_dockered_workflows":
+                            'dkfz_dockered_workflows':
                             {
-                                "name":"dkfz_dockered_workflows",
-                                "url":"https://s3.amazonaws.com/oicr.docker.private.images/dkfz_dockered_workflows_1.3.tar"
+                                'name':'dkfz_dockered_workflows',
+                                'url':'https://s3.amazonaws.com/oicr.docker.private.images/dkfz_dockered_workflows_1.3.tar'
                             }
                         },
-                        "ami_id":"ami-12345"
+                        'ami_id':'ami-12345'
                     }
                 }
 
@@ -140,24 +141,28 @@ class DaemonCommand(Command):
 
     def get_parser(self,prog_name):
         parser = super(DaemonCommand,self).get_parser(prog_name)
-        subparser = parser.add_subparsers(title='subcommands',help='coordinator subcommands: start, stop, restart',dest='subparser_name')
+        subparser = parser.add_subparsers(title='subcommands',help='coordinator subcommands: start, stop, restart',dest='service_state')
+        subparser.required=True
         subparser.add_parser('start',help='Start the '+self.service_name+' service')
         subparser.add_parser('stop',help='Stop the '+self.service_name+' service')
         subparser.add_parser('restart',help='Stop the '+self.service_name+' service')
         return parser
 
     def take_action(self, parsed_args):
-        subparser_name=vars(parsed_args)['subparser_name']
-        self.log.debug('subparser: %s',subparser_name)
-
-        #if subparser_name=='start':
-            #TODO: Figure out how to properly start/stop services from within python
-            # service Coordinator start
-        #elif subparser_name=='stop':
-            # service Coordinator stop
-        #elif subparser_name=='restart':
-            # service Coordinator restart
-
+        subparser_name=vars(parsed_args)['service_state']
+        print ('working on service: '+self.service_name)
+        self.log.info('subparser: %s',subparser_name)
+        start_cmd='flock -n /tmp/arch3_'+self.service_name+'.pid ' +self.service_name+' --config /home/ubuntu/arch3/config/masterConfig.ini --endless'
+        stop_cmd='pkill -f '+self.service_name
+        if subparser_name=='start':
+            print (start_cmd)
+            subprocess.Popen(start_cmd.split(' '))
+        elif subparser_name=='stop':
+            print (stop_cmd)
+            subprocess.call(stop_cmd.split(' '))
+        elif subparser_name=='restart':
+            subprocess.check_call(stop_cmd.split(' '))
+            subprocess.Popen(start_cmd.split(' '))
 
 class Coordinator(DaemonCommand):
     "The Coordinator will process existing job orders into requests for VMs and jobs that will be picked up by running VMs."
@@ -188,9 +193,9 @@ class Generator(Command):
             print('Oh, I\'m SO sorry, but '+workflow_name+' is not the name of an available workflow.\nPlease use the command \'workflows list\' to see the list of currently available workflows.')
         else:
             workflow_details = WorkflowLister.get_workflow_details(workflow_name)
-            generator_cmd = 'Generator --workflow-name '+workflow_name+' --workflow-version '+workflow_details['http_workflow']['version']+' --workflow-path '+'/workflows/'+workflow_details['full_name']+' --ini-dir '+'/home/ubuntu/ini-dir'
+            generator_cmd = 'Generator --workflow-name '+workflow_name+' --workflow-version '+workflow_details['http_workflow']['version']+' --workflow-path '+'/workflows/'+workflow_details['full_name']+' --ini-dir '+'/home/ubuntu/ini-dir --config /home/ubuntu/arch3/config/masterConfig.ini'
             print(generator_cmd)
-            #TODO execute generator_cmd in the background using the subprocess module
+            subprocess.check_call(generator_cmd.split(' '))
 
 
 class Reports(Command):

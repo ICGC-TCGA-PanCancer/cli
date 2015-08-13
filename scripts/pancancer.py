bdbb67379d228cb48267d8883f82a85e37577f75
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2015-08-13 15:35:42 -0400

    Add status checking

--- scripts/pancancer.py
+++ scripts/pancancer.py
@@ -163,10 +163,8 @@ class DaemonCommand(cliff.command.Command):
     service_name=''
     log = logging.getLogger(__name__)
 
-    def _do_get_status(self,status_cmd):
-        "Get the status of a process."
-
     def _do_start(self,start_cmd):
+        "Start a process, using flock to ensure that it can't be started multiple times."
         self.log.debug (start_cmd)
         pid_file_path='/tmp/arch3_'+self.service_name+'.pid'
         # set start_new_session=True so that we can kill flock and all it's child processes by killing the process group whose pgid is the same as the flock pid.
@@ -188,6 +186,7 @@ class DaemonCommand(cliff.command.Command):
 
 
     def _do_stop(self,stop_cmd):
+        "Stop a process."
         pid = ''
         # Read the pid from the file.
         pid_file_path='/tmp/arch3_'+self.service_name+'.pid'
@@ -331,6 +330,69 @@ class Reports(cliff.command.Command):
 
 ###
 
+class Status(cliff.command.Command):
+    "This will return status information about the launcher."
+    log = logging.getLogger(__name__)
+
+    def _do_sql_status(self, sql):
+        "Executes a command against the postgres database"
+        cmd = 'psql -U queue_user queue_status -c'
+        self.log.debug(cmd)
+        sql_cmd = cmd.split(' ')
+        sql_cmd.append(sql)
+        self.log.debug(sql_cmd)
+        subprocess.call(sql_cmd)
+
+    def _check_service_with_name(self, service_name):
+        "Uses pgrep to check if a process matches the pattern 'java.*<service_name>'"
+        # Use pgrep to check if the java process is running.
+        cmd = 'pgrep -fla java.*'+service_name
+        output=''
+        try:
+            output = subprocess.check_output(cmd.split(' '),universal_newlines=True)
+        except subprocess.CalledProcessError as e:
+            # If pgrep returns nothing, then the process is not running.
+            print ('The '+service_name+' process does not appear to be running.')
+
+        if output!='':
+            # Print the message with the PID
+            print ('The '+service_name+' appears to be running with PID: '+(output.split(' '))[0])
+
+    def _do_service_checks(self):
+        "Check the Coordinator and Provisioner."
+        self._check_service_with_name('Coordinator')
+        self._check_service_with_name('Provisioner')
+
+    def get_parser(self,prog_name):
+        parser = super(Status,self).get_parser(prog_name)
+        subparser = parser.add_subparsers(title='subcommands',help='status subcommands: queues, jobs, job_status',dest='status_subcmd')
+        subparser.required=True
+        subparser.add_parser('queues',help='Prints some basic status on the RabbitMQ queues.')
+        subparser.add_parser('job_status',help='Prints counts of jobs by status.')
+        subparser.add_parser('jobs',help='Prints some information from the job table.')
+        subparser.add_parser('services',help='Prints the status of the coordinator and provisioner')
+        return parser
+
+    def take_action(self,parsed_args):
+        subcmd = vars(parsed_args)['status_subcmd']
+        cmd=''
+        sql=''
+        if subcmd == 'queues':
+            #QueueStatus is a simple wrapper script around rabbitmq stats, as a part of architecture-setup
+            cmd = '/bin/bash QueueStats'
+            subprocess.call(cmd.split(' '))
+        elif subcmd == 'job_status':
+            #cmd = ('psql -U queue_user queue_status -c '.split(' ')).append('"select count(*), status from job group by status;"')
+            sql='select count(*), status from job group by status;'
+            self._do_sql_status(sql)
+        elif subcmd == 'jobs':
+            sql='select job_id, job_uuid, workflow, create_timestamp, update_timestamp from job;'
+            self._do_sql_status(sql)
+        elif subcmd == 'services':
+            self._do_service_checks()
+
+###
+
 class PancancerApp(cliff.app.App):
     log = logging.getLogger(__name__)
 
@@ -346,7 +408,8 @@ class PancancerApp(cliff.app.App):
             'generator': Generator,
             'reports': Reports,
             'provisioner': Provisioner,
-            'coordinator': Coordinator
+            'coordinator': Coordinator,
+            'status': Status
         }
         for k, v in commands.items():
             commandMgr.add_command(k, v)

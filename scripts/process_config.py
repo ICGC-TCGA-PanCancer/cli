1b82497e4b35eb367d0b41b395f7d7240f3935e5
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2015-08-26 15:30:16 -0400

    Fix path to config files; copy new config files to correct location

--- scripts/process_config.py
+++ scripts/process_config.py
@@ -1,6 +1,7 @@
 import json
 import pystache
 import os
+import shutil
 
 def makeConfigString(k,v):
     return k+'='+v+'\n'
@@ -15,13 +16,13 @@ def processYouxiaSettings(d):
         if not (isinstance(v,dict)):
             outstr+=makeConfigString(k,v)
         else:
-            if k=='aws':
-                # Process AWS-specific variables in the AWS heading
-                # awsStr+='\n[aws]\n'
-                aws_settings=d['aws']
-                for k1,v1 in aws_settings.items():
-                    aws_str+=makeConfigString(k1,str(v1))
-            elif k=='openstack':
+            # if k=='aws':
+            #     # Process AWS-specific variables in the AWS heading
+            #     # awsStr+='\n[aws]\n'
+            #     aws_settings=d['aws']
+            #     for k1,v1 in aws_settings.items():
+            #         aws_str+=makeConfigString(k1,str(v1))
+            if k=='openstack':
                 # Process AWS-specific variables in the AWS heading
                 openstack_str+='\n[openstack]\n'
                 openstack_settings=d['openstack']
@@ -57,16 +58,17 @@ def processConsonanceSettings(d):
 # Main body of script #
 #######################
 
-def main():
+def main(config_path):
     #TODO: This should take a path to any config file.
-    with open('simple_pancancer_config.json') as simple_config_file:
+    with open(config_path) as simple_config_file:
         simple_config=json.load(simple_config_file)
         # These are not something the user will put into their simple config,
         # so we need to get it from the environment (it will have been set by the start_services_in_container.sh script)
         simple_config['sensu_server_ip_address'] = os.environ['SENSU_SERVER_IP_ADDRESS']
         simple_config['queue_host'] = os.environ['SENSU_SERVER_IP_ADDRESS']
 
-    with open('pancancer_config.mustache') as mustache_template_file:
+    config_path = os.path.dirname(__file__)
+    with open(config_path +'/pancancer_config.mustache') as mustache_template_file:
         mustache_template=mustache_template_file.read()
 
     renderer=pystache.Renderer()
@@ -75,23 +77,27 @@ def main():
     data=json.loads(rendered_str)
 
     # The master config file should be written to ~/.pancancer/pancancer_config.json
-    with open('pancancer_config.json','w') as master_config_file:
+    with open(config_path + '/pancancer_config.json','w') as master_config_file:
         master_config_file.write(str(json.dumps(data,sort_keys=True, indent=4) ))
 
     # Youxia config should go to ~/.youxia/config
-    with open('youxia_config','w') as youxia_file:
+    with open(config_path + '/youxia_config','w') as youxia_file:
         youxia_settings=data['youxia']
         youxia_str=processYouxiaSettings(youxia_settings)
         youxia_file.write(youxia_str)
 
     # params.json should go to ~/params.json
-    with open('params.json','w') as params_json_file:
+    with open(config_path + '/params.json','w') as params_json_file:
         params_settings=data['params']
         params_str=processParams(params_settings)
         params_json_file.write(params_str)
 
     # masterConfig should go to ~/arch3/config/masterConfig.ini
-    with open('masterConfig.ini','w') as consonance_file:
+    with open(config_path + '/masterConfig.ini','w') as consonance_file:
         consonance_settings=data['consonance']
         consonance_str=processConsonanceSettings(consonance_settings)
         consonance_file.write(consonance_str)
+
+    shutil.copy2(config_path + '/youxia_config','/home/ubuntu/.youxia/config')
+    shutil.copy2(config_path + '/masterConfig.ini','/home/ubuntu/arch3/config/masterConfig.ini')
+    shutil.copy2(config_path + '/params.json','/home/ubuntu/params.json')

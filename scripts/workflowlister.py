2c55aacb7105151744c87b325596706555137580
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2015-09-30 15:05:13 -0400

    change AMI ID for HelloWorld; temporarily block other workflows

--- scripts/workflowlister.py
+++ scripts/workflowlister.py
@@ -16,88 +16,88 @@ class WorkflowLister:
                                 'image_name': 'pancancer/seqware_whitestar_pancancer:1.1.1'
                             }
                         },
-                        'ami_id':'ami-270cc34c',
+                        'ami_id':'ami-791c5b1c',
                         'default-ini':'http://something.ini',
                         'instance-type':'m1.xlarge',
                         'lvm_devices':'/dev/xvdb,/dev/xvdc,/dev/xvdd,/dev/xvde'
-                    },
-                    'Sanger':
-                    {
-                        'full_name':'Workflow_Bundle_SangerPancancerCgpCnIndelSnvStr_1.0.8_SeqWare_1.1.0',
-                        'http_workflow':
-                        {
-                            'url':'https://s3.amazonaws.com/oicr.workflow.bundles/released-bundles/Workflow_Bundle_SangerPancancerCgpCnIndelSnvStr_1.0.8_SeqWare_1.1.0.zip',
-                            'version':'1.0.8'
-                        },
-                        'containers':
-                        {
-                            'seqware_whitestar_pancancer': {
-                                'name':'seqware_whitestar_pancancer',
-                                'image_name': 'pancancer/seqware_whitestar_pancancer:1.1.1'
-                            }
-                        },
-                        'ami_id':'ami-270cc34c',
-                        'default-ini':'https://raw.githubusercontent.com/ICGC-TCGA-PanCancer/SeqWare-CGP-SomaticCore/1.0.8/workflow/config/CgpCnIndelSnvStrWorkflow.ini',
-                        'instance-type':'m1.xlarge',
-                        'lvm_devices':'/dev/xvdb,/dev/xvdc,/dev/xvdd,/dev/xvde'
-                    },
-                    'BWA':
-                    {
-                        'full_name':'Workflow_Bundle_BWA_2.6.5_SeqWare_1.1.1',
-                        'http_workflow':
-                        {
-                            'url':'https://s3.amazonaws.com/oicr.workflow.bundles/released-bundles/Workflow_Bundle_BWA_2.6.5_SeqWare_1.1.1.zip',
-                            'version':'2.6.5'
-                        },
-                        'containers':
-                        {
-                            'seqware_whitestar_pancancer': {
-                                'name':'seqware_whitestar_pancancer',
-                                'image_name': 'pancancer/seqware_whitestar_pancancer:1.1.1'
-                            }
-                        },
-                        'ami_id':'ami-270cc34c',
-                        'default-ini':'https://raw.githubusercontent.com/ICGC-TCGA-PanCancer/Seqware-BWA-Workflow/2.6.5/workflow/config/workflow.ini',
-                        'instance-type':'m1.xlarge',
-                        'lvm_devices':'/dev/xvdb,/dev/xvdc,/dev/xvdd,/dev/xvde'
-                    },
-                    'DKFZ_EMBL':
-                    {
-                        'full_name':'Workflow_Bundle_DEWrapperWorkflow_1.0.5_SeqWare_1.1.1',
-                        'http_workflow':
-                        {
-                            'url':'https://s3.amazonaws.com/oicr.workflow.bundles/released-bundles/Workflow_Bundle_DEWrapperWorkflow_1.0.5_SeqWare_1.1.1.zip',
-
-                            'version':'1.0.5'
-                        },
-                        'containers':
-                        {
-                            'pcawg-delly-workflow': {
-                                'name':'pcawg-delly-workflow',
-                                'image_name': 'pancancer/pcawg-delly-workflow:1.0'
-                            },
-                            'seqware_whitestar_pancancer': {
-                                'name':'seqware_whitestar_pancancer',
-                                'image_name': 'pancancer/seqware_whitestar_pancancer:1.1.1'
-                            },
-                            'pancancer_upload_download': {
-                                'name':'pancancer_upload_download',
-                                'image_name':'pancancer/pancancer_upload_download:1.2'
-                            }
-                        },
-                        's3_containers':
-                        {
-                            'dkfz_dockered_workflows':
-                            {
-                                'name':'dkfz_dockered_workflows',
-                                'url':'s3://oicr.docker.private.images/dkfz_dockered_workflows_1.3.tar'
-                            }
-                        },
-                        'ami_id':'ami-270cc34c',
-                        'default-ini':'https://raw.githubusercontent.com/ICGC-TCGA-PanCancer/DEWrapperWorkflow/1.0.6/workflow/config/DEWrapperWorkflow.ini',
-                        'instance-type':'m1.xlarge',
-                        'lvm_devices':'/dev/xvdb,/dev/xvdc,/dev/xvdd,/dev/xvde'
                     }
+#                     ,'Sanger':
+#                     {
+#                         'full_name':'Workflow_Bundle_SangerPancancerCgpCnIndelSnvStr_1.0.8_SeqWare_1.1.0',
+#                         'http_workflow':
+#                         {
+#                             'url':'https://s3.amazonaws.com/oicr.workflow.bundles/released-bundles/Workflow_Bundle_SangerPancancerCgpCnIndelSnvStr_1.0.8_SeqWare_1.1.0.zip',
+#                             'version':'1.0.8'
+#                         },
+#                         'containers':
+#                         {
+#                             'seqware_whitestar_pancancer': {
+#                                 'name':'seqware_whitestar_pancancer',
+#                                 'image_name': 'pancancer/seqware_whitestar_pancancer:1.1.1'
+#                             }
+#                         },
+#                         'ami_id':'ami-270cc34c',
+#                         'default-ini':'https://raw.githubusercontent.com/ICGC-TCGA-PanCancer/SeqWare-CGP-SomaticCore/1.0.8/workflow/config/CgpCnIndelSnvStrWorkflow.ini',
+#                         'instance-type':'m1.xlarge',
+#                         'lvm_devices':'/dev/xvdb,/dev/xvdc,/dev/xvdd,/dev/xvde'
+#                     },
+#                     'BWA':
+#                     {
+#                         'full_name':'Workflow_Bundle_BWA_2.6.5_SeqWare_1.1.1',
+#                         'http_workflow':
+#                         {
+#                             'url':'https://s3.amazonaws.com/oicr.workflow.bundles/released-bundles/Workflow_Bundle_BWA_2.6.5_SeqWare_1.1.1.zip',
+#                             'version':'2.6.5'
+#                         },
+#                         'containers':
+#                         {
+#                             'seqware_whitestar_pancancer': {
+#                                 'name':'seqware_whitestar_pancancer',
+#                                 'image_name': 'pancancer/seqware_whitestar_pancancer:1.1.1'
+#                             }
+#                         },
+#                         'ami_id':'ami-270cc34c',
+#                         'default-ini':'https://raw.githubusercontent.com/ICGC-TCGA-PanCancer/Seqware-BWA-Workflow/2.6.5/workflow/config/workflow.ini',
+#                         'instance-type':'m1.xlarge',
+#                         'lvm_devices':'/dev/xvdb,/dev/xvdc,/dev/xvdd,/dev/xvde'
+#                     },
+#                     'DKFZ_EMBL':
+#                     {
+#                         'full_name':'Workflow_Bundle_DEWrapperWorkflow_1.0.5_SeqWare_1.1.1',
+#                         'http_workflow':
+#                         {
+#                             'url':'https://s3.amazonaws.com/oicr.workflow.bundles/released-bundles/Workflow_Bundle_DEWrapperWorkflow_1.0.5_SeqWare_1.1.1.zip',
+# 
+#                             'version':'1.0.5'
+#                         },
+#                         'containers':
+#                         {
+#                             'pcawg-delly-workflow': {
+#                                 'name':'pcawg-delly-workflow',
+#                                 'image_name': 'pancancer/pcawg-delly-workflow:1.0'
+#                             },
+#                             'seqware_whitestar_pancancer': {
+#                                 'name':'seqware_whitestar_pancancer',
+#                                 'image_name': 'pancancer/seqware_whitestar_pancancer:1.1.1'
+#                             },
+#                             'pancancer_upload_download': {
+#                                 'name':'pancancer_upload_download',
+#                                 'image_name':'pancancer/pancancer_upload_download:1.2'
+#                             }
+#                         },
+#                         's3_containers':
+#                         {
+#                             'dkfz_dockered_workflows':
+#                             {
+#                                 'name':'dkfz_dockered_workflows',
+#                                 'url':'s3://oicr.docker.private.images/dkfz_dockered_workflows_1.3.tar'
+#                             }
+#                         },
+#                         'ami_id':'ami-270cc34c',
+#                         'default-ini':'https://raw.githubusercontent.com/ICGC-TCGA-PanCancer/DEWrapperWorkflow/1.0.6/workflow/config/DEWrapperWorkflow.ini',
+#                         'instance-type':'m1.xlarge',
+#                         'lvm_devices':'/dev/xvdb,/dev/xvdc,/dev/xvdd,/dev/xvde'
+#                     }
                 }
 
     @staticmethod

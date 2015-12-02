4170a83b132e985936ea20d108e358ff52a553cc
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2015-12-02 16:31:01 -0500

    Create ~/.keystore if it does not exist.

--- scripts/azure/create_management_certs.sh
+++ scripts/azure/create_management_certs.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
-#pass=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1)
-#echo $pass
+
 pass=launcherKeystore
 datestr=$(date +%Y%m%d%H%M%S)
 
-echo "generate keypair"
+# create the directory if it doesn't exist.
+if [ ! -d ~/.keystore ] ; then
+    mkdir ~/.keystore
+fi
+
+echo "Now generating keypair..."
 keytool -genkey -alias launcher_keystore \
         -keystore ~/.keystore/launcher.pfx \
         -storepass "$pass" \
@@ -15,17 +19,17 @@ keytool -genkey -alias launcher_keystore \
         -keysize 2048 \
         -dname "cn=Self Signed Certificate $datestr, ou=Launcher, o=Launcher, L=AzureCloud, S=NA, C=US"
 
-echo "generate cert"
+echo "Now generating certificate file..."
 keytool -export -alias launcher_keystore \
         -storetype pkcs12 \
         -keystore ~/.keystore/launcher.pfx \
         -storepass $pass \
         -rfc \
-        -file ~/.keystore/launcher.cert
+        -file ~/.keystore/launcher.cer
 
-echo "Now you have to manually upload ~/.keystore/launcher_key.cert to the classic Azure portal."
+echo "Now you have to manually upload ~/.keystore/launcher.cer to the classic Azure portal."
 
-echo "generating jks file"
+echo "Now generating Java KeyStore..."
 keytool -importkeystore \
         -srckeystore ~/.keystore/launcher.pfx \
         -destkeystore ~/.keystore/launcher.jks \
@@ -36,7 +40,6 @@ keytool -importkeystore \
         -srckeypass $pass \
         -destkeypass $pass \
         -alias launcher_keystore
-
 
 echo "The JKS file has now been created at ~/.keystore/launcher.jks You can now reference it in ~/.youxia/config"
 
